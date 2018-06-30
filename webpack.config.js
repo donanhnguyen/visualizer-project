42b1a5b2aec842272c709e1bac58232bc7a2a50b
Don Nguyen <thedonwind69@gmail.com>
2018-06-29 21:17:08 -0700

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
+var path = require("path");
+
 module.exports = {
-    entry: ".entry.jsx",
-    output: {
-      filename: "./js/bundle.js"
-    }, 
-    watch: true
-}
+  context: __dirname,
+  entry: "./entry.jsx",
+  output: {
+    filename: "bundle.js"
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'cheap-eval-source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*", ".jpg"]
+  }
+};
