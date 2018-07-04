b4cd26bedcf32ae199a3e1176934e6ae81b6dba6
Don Nguyen <thedonwind69@gmail.com>
2018-07-03 20:12:24 -0700

    add

--- components/city_index_item.jsx
+++ components/city_index_item.jsx
@@ -14,16 +14,18 @@ class CityIndexItem extends React.Component {
         super(props);
         this.state = {
             currentCityIndex: this.props.match.params.cityId,
-            currentCity: Cities[this.props.match.params.cityId]
+            currentCity: Cities[this.props.match.params.cityId],
+            currentCityPopulation: Object.keys(Cities[this.props.match.params.cityId].population).map((key) => ({[key]: Cities[this.props.match.params.cityId].population[key]}))
         }
     }
 
     render () {
         var currentCity = this.state.currentCity;
+        console.log(this.state.currentCityPopulation);
         return (
             <div>
                 <button class='qbutton'><Link class='city-link' to='/'>Back to home page</Link></button>
-                <h1>5 year population growth from 2012-2016 for {currentCity.name}</h1>
+                <h1>9 year population growth from 2007-2016 for {currentCity.name}</h1>
                 <div id="bar-graph">
 
                 </div>
