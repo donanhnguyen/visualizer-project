0e369796fdd78265298d722dafee505d5c7f9083
Don Nguyen <thedonwind69@gmail.com>
2018-07-06 22:46:33 -0700

    yes

--- components/city_index_item.jsx
+++ components/city_index_item.jsx
@@ -16,12 +16,23 @@ class CityIndexItem extends React.Component {
         this.state = {
             currentCityIndex: this.props.match.params.cityId,
             currentCity: Cities[this.props.match.params.cityId],
-            currentCityPopulation: Object.keys(Cities[this.props.match.params.cityId].population).map((key) => ({[key]: Cities[this.props.match.params.cityId].population[key]}))
+            // currentCityPopulation: Object.keys(Cities[this.props.match.params.cityId].population).map((key) => ({[key]: Cities[this.props.match.params.cityId].population[key]}))
+            currentCityPopulation: Object.keys(Cities[this.props.match.params.cityId].population).map((key) => (Cities[this.props.match.params.cityId].population[key]))
         }
     }
 
+    componentDidMount() {
+        var array = [5, 10, 15, 20, 25];
+        D3Stuff.createBarChart(this.state.currentCityPopulation);
+    }
+
+    // componentWillUnmount() {
+    //     D3Stuff.refreshBarChart();
+    // }
+
     render () {
         var currentCity = this.state.currentCity;
+        console.log(this.state.currentCityPopulation);
         return (
             <div>
                 <button class='qbutton'><Link class='city-link' to='/'>Back to home page</Link></button>
--- js/d3-stuff.js
+++ js/d3-stuff.js
@@ -1,6 +1,18 @@
-export const d3test = () => {
-   var body = d3.select("body");
-    var div = body.append("div");
-    div.html("Hello, world!"); 
+export const createBarChart = (array) => {
+    d3.select("#bar-graph").selectAll("div")
+        .data(array)
+        .enter()
+        .append("div")
+        .attr("class", "bar-chart")
+        .style("height", function (d) {
+            var pop = parseFloat(d.replace(/,/g, ''));
+            var barHeight = (pop / 10000 );
+            console.log(parseFloat(d.replace(/,/g, '')) / 10000);
+            return barHeight + "px";
+        });
+};
+
+export const refreshBarChart = () => {
+    d3.select("#bar-graph").html("");
 };
 
