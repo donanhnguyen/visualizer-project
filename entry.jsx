4816cd6188e6a0dc603a74c9bb28c9f347e2a6af
Don Nguyen <thedonwind69@gmail.com>
2018-07-06 21:48:50 -0700

    change

--- entry.jsx
+++ entry.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from "./components/root";
-import Cities from "./js/cities";
 
 document.addEventListener("DOMContentLoaded", () => {
     var root = document.getElementById("root");
     ReactDOM.render(<Root />, root);
-
 });
