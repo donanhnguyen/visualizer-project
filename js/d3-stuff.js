f1dc9496dde699084b34a036c4fa95317ae27935
Don Nguyen <thedonwind69@gmail.com>
2018-07-16 20:44:12 -0700

    add

--- js/d3-stuff.js
+++ js/d3-stuff.js
@@ -19,9 +19,12 @@
 
 export const createBarChart = (array) => {
 
+var colors = d3.schemeCategory10;
+console.log(d3);
 var svg = d3.select("#svg");
 
 var padding = {top: 20, right: 30, bottom: 30, left: 80};
+
 var chartArea = {
     "width": parseInt(svg.style("width")) - padding.left - padding.right,
     "height": parseInt(svg.style("height")) - padding.top - padding.bottom
@@ -29,7 +32,7 @@ var chartArea = {
 
 
 var yScale = d3.scaleLinear()
-    .domain([0, d3.max(array, function(d, i) {return parseInt(d.pop)  } )   ])
+    .domain([0, d3.max(array, function(d, i) {return parseInt(parseFloat((d.pop.replace(/,/g, ''))))  } )   ])
     .range([chartArea.height, 0]).nice();
   
 var xScale = d3.scaleBand()
@@ -55,23 +58,42 @@ yAxisFn(yAxis);
 var rectGrp = svg.append("g")
     .attr(
         "transform", "translate ("+ padding.left + "," + padding.top + ")"
-    )
+    );
+
+rectGrp.selectAll("rect")
+    .data(array)
+    .enter()
+    .append("rect")
+    .attr("width", xScale.bandwidth())
+    .attr("height", function (d, i) {
+        return chartArea.height - yScale(parseFloat((d.pop.replace(/,/g, ''))));
+    } )
+    .attr("x", function (d, i) {
+        return xScale(d.year);
+    })
+    .attr("y", function (d, i) {
+        return yScale(parseFloat((d.pop.replace(/,/g, ''))));
+    })
+    .attr("fill", function (d, i) {
+        return colors[i];
+    })
+    .attr("class", "bar")
+
+rectGrp.selectAll("text")
+    .data(array)
+    .enter()
+    .append("text")
+    .attr("x", function (d) {return xScale(d.year)})
+    .attr("y", function (d) { return yScale(parseFloat((d.pop.replace(/,/g, ''))))})
+    .attr("dy", "0em")
+    .text(function(d, i) {
+        return d.pop;
+    })
+    .attr("style", "font-size: 20px")
+
+
 
 
 };
-
-// d3.select("#bar-graph").selectAll("div")
-//         .data(array)
-//         .enter()
-//         .append("div")
-//         .attr("class", "bar")
-//         .style("height", function (d) {
-//             var pop = parseFloat((d.pop.replace(/,/g, '')));
-//             var barHeight = (pop / 10000 ) + 80;
-//             return barHeight + "px";
-//         })
-//         .html(function (d) {return d.pop} )
-        
-//         ;
 
 
