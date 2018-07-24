fb2fc642b8c076657797e06f110d84170bfa152e
Don Nguyen <thedonwind69@gmail.com>
2018-07-23 21:06:08 -0700

    sup

--- js/cities.js
+++ js/cities.js
@@ -13,6 +13,13 @@ const Cities = [
         {year: '2014', pop: "911,390"},
         {year: '2015', pop: "930,152"},
         {year: '2016', pop: "947,890"},
+    ],
+    demographics: [
+        {race: "White", percentage: 48},
+        {race: "Hispanic", percentage: 35},
+        {race: "Black", percentage: 8},
+        {race: "Asian", percentage: 6},
+        {race: "Other", percentage: 3},
     ]
 }
 ,
@@ -30,6 +37,13 @@ const Cities = [
         {year: '2014', pop: "667,963"},
         {year: '2015', pop: "683,505"},
         {year: '2016', pop: "704,352"},
+    ],
+    demographics: [
+        {race: "White", percentage: 66},
+        {race: "Hispanic", percentage: 6},
+        {race: "Black", percentage: 8},
+        {race: "Asian", percentage: 13},
+        {race: "Other", percentage: 7},
     ]
 }
 ,
@@ -47,6 +61,13 @@ const Cities = [
         {year: '2014', pop: "808,362"},
         {year: '2015', pop: "826,395"},
         {year: '2016', pop: "842,051"},
+    ],
+    demographics: [
+        {race: "White", percentage: 45},
+        {race: "Hispanic", percentage: 13},
+        {race: "Black", percentage: 35},
+        {race: "Asian", percentage: 5},
+        {race: "Other", percentage: 2},
     ]
 }
 ,
@@ -65,6 +86,13 @@ const Cities = [
         {year: '2014', pop: "662,425"},
         {year: '2015', pop: "680,032"},
         {year: '2016', pop: "693,060"},
+    ],
+    demographics: [
+        {race: "White", percentage: 52},
+        {race: "Hispanic", percentage: 32},
+        {race: "Black", percentage: 10},
+        {race: "Asian", percentage: 3},
+        {race: "Other", percentage: 3},
     ]
 }
 ,
@@ -83,6 +111,13 @@ const Cities = [
         {year: '2014', pop: "850,424"},
         {year: '2015', pop: "862,004"},
         {year: '2016', pop: "870,887"},
+    ],
+    demographics: [
+        {race: "White", percentage: 41},
+        {race: "Hispanic", percentage: 15},
+        {race: "Black", percentage: 6},
+        {race: "Asian", percentage: 35},
+        {race: "Other", percentage: 3},
     ]
 
 }
