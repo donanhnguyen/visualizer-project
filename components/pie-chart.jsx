16c7212cc1875c9eedb5f34c5aff3f018c49c6e2
Don Nguyen <thedonwind69@gmail.com>
2018-07-25 20:04:52 -0700

    sex

--- components/pie-chart.jsx
+++ components/pie-chart.jsx
@@ -7,6 +7,7 @@ import {
     HashRouter
 } from 'react-router-dom';
 import Cities from "../js/cities";
+import createPieChart from '../js/createPieChart';
 
 class PieChart extends React.Component {
 
