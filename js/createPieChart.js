eb368cc7f145723c2208297129edf003f6d345f2
Don Nguyen <thedonwind69@gmail.com>
2018-07-26 21:03:44 -0700

    yes

--- js/createPieChart.js
+++ js/createPieChart.js
@@ -17,8 +17,12 @@ const createPieChart = (array) => {
     .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
 
     var arc = d3.arc()
-    .innerRadius(0)
+    .innerRadius(150)
     .outerRadius(radius);
+
+    var labelArc = d3.arc()
+        .outerRadius(radius - 50)
+        .innerRadius(radius - 50)
 
     var pie = d3.pie()
     .value(function(d) { return d.percentage; })
@@ -34,10 +38,39 @@ const createPieChart = (array) => {
     })
     .attr('class', 'pie-slice')
 
+    path.transition()
+    .duration(1000)
+    .attrTween('d', function(d) {
+        var interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, d);
+        return function(t) {
+            return arc(interpolate(t));
+        };
+    });
+
+    restOfTheData(svg, pie, array, arc);
+
 
 
 
     
+};
+
+const restOfTheData = (svg, pie, array, arc) => {
+    var text = svg.selectAll('text')
+    .data(pie(array))
+    .enter()
+    .append("text")
+    .transition()
+    .duration(1000)
+    .attr("transform", function (d) {
+        return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("dy", ".60em")
+    .attr("text-anchor", "middle")
+    .text(function(d){
+        return d.data.percentage + "%";
+    })
+    .attr("style", "font-size: 15px; fill: white")
 };
 
 export default createPieChart;
