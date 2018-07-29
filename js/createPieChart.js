d20e4f455e53414c2e04a986b933da7a0648d929
Don Nguyen <thedonwind69@gmail.com>
2018-07-29 11:00:27 -0700

    stu

--- js/createPieChart.js
+++ js/createPieChart.js
@@ -1,13 +1,14 @@
-var d3 = require("./d3.js");
+// var d3 = require("./d3.js");
 
 const createPieChart = (array) => {
-
+    console.log(d3);
     var width = 440;
     var height = 440;
     var radius = Math.min(width, height) / 2;
 
-    var color = ["blue", "green", "red", "orange", "purple", "gray"];
-
+    // var color = ["blue", "green", "red", "orange", "purple", "gray"];
+    var color = d3.schemeCategory10
+    var color1 = d3.scaleOrdinal(d3.schemeCategory10);
     var svg = d3.select('#pie-chart')
     .append('svg')
     .attr('width', width)
@@ -42,11 +43,7 @@ const createPieChart = (array) => {
         };
     });
 
-    restOfTheData(svg, pie, array, arc);
-
-};
-
-const restOfTheData = (svg, pie, array, arc) => {
+    //text
     var text = svg.selectAll('text')
     .data(pie(array))
     .enter()
@@ -62,6 +59,50 @@ const restOfTheData = (svg, pie, array, arc) => {
         return d.data.percentage + "%";
     })
     .attr("style", "font-size: 15px; fill: white")
+
+    //legend
+    var legendRectSize=20;
+    var legendSpacing=7;
+    var legendHeight=legendRectSize+legendSpacing;
+ 
+ 
+    var legend=svg.selectAll('.legend')
+        .data(array)
+        .enter()
+        .append('g')
+        .attr({
+            class:'legend',
+            transform:function(d,i){
+                //Just a calculation for x & y position
+                return 'translate(-35,' + ((i*legendHeight)-65) + ')';
+            }
+        });
+
+        console.log(legend);
+    // legend.append('rect')
+    //     .attr({
+    //         width:legendRectSize,
+    //         height:legendRectSize,
+    //         rx:20,
+    //         ry:20
+    //     })
+    //     .style({
+    //         fill: function (d, i) {return color[i]},
+    //         stroke: function (d, i) {return color[i]}
+    //     });
+ 
+    // legend.append('text')
+    //     .attr({
+    //         x:30,
+    //         y:15
+    //     })
+    //     .text(function(d){
+    //         return d.data.race;
+    //     }).style({
+    //         fill:'#929DAF',
+    //         'font-size':'14px'
+    //     }); 
+
 };
 
 export default createPieChart;
